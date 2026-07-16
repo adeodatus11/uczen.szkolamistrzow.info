b045adebcff39b21ca883959a2f0d48526dc31e9
mnajwer <demotyczny.rowerem_0x@icloud.com>
2026-07-16 12:07:19 +0200

    Fix asset paths for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-const githubPages = process.env.GITHUB_ACTIONS === "true";
-
 export default defineConfig({
-  site: githubPages ? "https://adeodatus11.github.io" : "https://uczen.szkolamistrzow.info",
-  base: githubPages ? "/uczen.szkolamistrzow.info" : "/",
+  site: "https://informator.szkolamistrzow.info",
+  base: "/",
   output: "static",
   integrations: [sitemap()],
 });
